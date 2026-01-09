1cdc3c91be06cd3cc633d9321327839b252bb26a
Henri Buhlmann <henri.buhlmann.business@gmail.com>
2026-01-09 23:24:29 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,12 +6,11 @@ def main():
     while True:
         sys.stdout.write("$ ")
         user_input = input()
+        if user_input == "exit":
+            break
         print(f"{user_input}: command not found")
         
-
-
-
-
+        
 if __name__ == "__main__":
     main()
 
