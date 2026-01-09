9de3e2f649dec3b3075a784cb4acfdaaf6065e9a
Henri Buhlmann <henri.buhlmann.business@gmail.com>
2026-01-09 23:59:53 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,11 +5,22 @@ def main():
 
     while True:
         sys.stdout.write("$ ")
-        user_input = input()
-        if user_input == "exit":
+        user_input = input().split()
+        if len(user_input) == 0:
+            continue
+        else:
+            command = user_input[0]
+            arguments = user_input[1:]
+        if command == "exit":
             break
-        print(f"{user_input}: command not found")
+        elif command == "echo":
+            print(" ".join(arguments))
+        else:
+            print(f"{command}: command not found")       
         
+        
+
+      
         
 if __name__ == "__main__":
     main()
